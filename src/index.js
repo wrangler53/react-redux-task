44404ae2c4a56e83af123d63b2787adfa675ec16
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2019-01-28 18:54:46 +0200

    Configured SCSS usage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import './styles/scss/index.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
