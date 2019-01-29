6af648b89a5780f95c15a14e7e06fd1a5322eab0
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2019-01-29 03:12:39 +0200

    Render candidate cards on board depends on their status

--- src/components/BoardColumn/index.js
+++ src/components/BoardColumn/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const BoardColumn = ({ name, index }) => (
+import CandidateCard from '../CandidateCard';
+
+const BoardColumn = ({ name, candidates }) => (
   <div className="board-column">
     <div className="board-column__header">{name}</div>
-    <div className="board-column__items"></div>
+    <div className="board-column__items">
+      {
+        candidates.map(candidate =>
+          <CandidateCard
+            key={candidate.id.value}
+            candidate={candidate}
+          />
+        )
+      }
+    </div>
   </div>
 );
 
-export default BoardColumn;
+const mapStateToProps = ({ candidatesReducer }, ownProps) => ({
+  candidates: candidatesReducer.filter(({ status }) => status === ownProps.index)
+});
+
+export default connect(mapStateToProps)(BoardColumn);
---
+++ src/components/CandidateCard/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CandidateCard = () => (
+  <div className="candidate-card"></div>
+)
+
+export default CandidateCard;
